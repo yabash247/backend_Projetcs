07b6d2c5af11c00a629d147d19a82fb5beaf8ab9
yabash247 <yabashllc@gmail.com>
2025-02-01 23:15:24 -0600

    url updated

--- backend/settings.py
+++ backend/settings.py
@@ -64,7 +64,9 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "https://yabash.com",
     "http://yabash.com",
-    "https://www.yabash.com"
+    "https://www.yabash.com",
+    "http://localhost:3000/edmin",
+
 ]
 
 
